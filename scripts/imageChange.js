299229d4f342c2251263859dddf89732e355e143
Matt Allinson <mrallinson@gmail.com>
2021-03-15 23:07:30 +0200

    init commit

---
+++ scripts/imageChange.js
@@ -0,0 +1,8 @@
+<script>
+	function changeSrc() {
+	    if (document.getElementById("raw").checked) {
+	    document.getElementById("picture").src = "https://pbs.twimg.com/profile_images/447374371917922304/P4BzupWu.jpeg";
+	    } else if (document.getElementById("kittybutton").checked) { 
+	    document.getElementById("picture").src = "http://cutewallpaperss.com/wp-content/uploads/2015/01/cute_cats__cats_picture_cute_wallpaperss_hd_for_mobile.jpg";
+	    }
+	}
